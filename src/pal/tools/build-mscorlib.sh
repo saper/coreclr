875cba58b52d6544654f0dd783f7217ad5492e40
Marcin Cieslak <saper@saper.info>
2015-06-26 23:34:36 +0000

    nuget.exe: download, set destination path
    Download nuget.exe with a shell script
    
    Use $(LocalToolsDir) workaround to get
    NuGet download path correct

--- src/pal/tools/build-mscorlib.sh
+++ src/pal/tools/build-mscorlib.sh
@@ -7,13 +7,91 @@ CLEAN="/t:rebuild"
 
 BINDIR="bin"
 LOGDIR="${BINDIR}/Logs"
+TOOLSDIR="${BINDIR}/tools"
 LOGFILE="${LOGDIR}/mscorlib_${OS}_${ARCH}_${BUILDTYPE}.log"
+mkdir -p "${LOGDIR}" "${TOOLSDIR}"
 
-exec /usr/local/bin/mono --debug /usr/local/lib/mono/xbuild/12.0/bin/xbuild.exe build.proj \
-	${CLEAN} /nologo /verbosity:minimal /fileloggerparameters:LogFile=${LOGFILE} \
-	/p:OS=${OS} /p:__BuildOS=${OS} /p:BuildNugetPackage=false \
-	/p:CLRRuntime=/usr/local/bin/mono \
-	/p:SDK40ToolsPath=/usr/local/lib/mono/4.5/ \
-	/p:GCC=gcc48 /p:UseMonoCompiler=true \
-	/p:WindowsSdkDir=/usr/local/mingw32
 
+main() {
+    
+  exec xbuild build.proj \
+    ${CLEAN} /nologo /verbosity:diag /fileloggerparameters:LogFile=${LOGFILE} \
+    /p:OS=${OS} /p:__BuildOS=${OS} /p:BuildNugetPackage=false \
+    /p:CLRRuntime=/usr/local/bin/mono \
+    /p:SDK40ToolsPath=/usr/local/lib/mono/4.5/ \
+    /p:GCC=gcc48 /p:UseMonoCompiler=true \
+    /p:WindowsSdkDir=/usr/local/mingw32 \
+    /p:LocalToolsDir=../bin/tools
+}
+
+nugetdownload() {
+  URL=https://www.nuget.org/nuget.exe
+  
+  if [ -z "$1" ]; then
+  	>&2 echo "nugetdownload: Need output directory for nuget.exe"
+  	exit 2
+  fi
+  
+  mkdir -p "$1"
+  cert_dir="$1"
+  output_dir="$1"
+  
+  cat > ${cert_dir}/nugetroot.pem <<CERT1
+-----BEGIN CERTIFICATE-----
+MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBs
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSswKQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5j
+ZSBFViBSb290IENBMB4XDTA2MTExMDAwMDAwMFoXDTMxMTExMDAwMDAwMFowbDEL
+MAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3
+LmRpZ2ljZXJ0LmNvbTErMCkGA1UEAxMiRGlnaUNlcnQgSGlnaCBBc3N1cmFuY2Ug
+RVYgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMbM5XPm
++9S75S0tMqbf5YE/yc0lSbZxKsPVlDRnogocsF9ppkCxxLeyj9CYpKlBWTrT3JTW
+PNt0OKRKzE0lgvdKpVMSOO7zSW1xkX5jtqumX8OkhPhPYlG++MXs2ziS4wblCJEM
+xChBVfvLWokVfnHoNb9Ncgk9vjo4UFt3MRuNs8ckRZqnrG0AFFoEt7oT61EKmEFB
+Ik5lYYeBQVCmeVyJ3hlKV9Uu5l0cUyx+mM0aBhakaHPQNAQTXKFx01p8VdteZOE3
+hzBWBOURtCmAEvF5OYiiAhF8J2a3iLd48soKqDirCmTCv2ZdlYTBoSUeh10aUAsg
+EsxBu24LUTi4S8sCAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQF
+MAMBAf8wHQYDVR0OBBYEFLE+w2kD+L9HAdSYJhoIAu9jZCvDMB8GA1UdIwQYMBaA
+FLE+w2kD+L9HAdSYJhoIAu9jZCvDMA0GCSqGSIb3DQEBBQUAA4IBAQAcGgaX3Nec
+nzyIZgYIVyHbIUf4KmeqvxgydkAQV8GK83rZEWWONfqe/EW1ntlMMUu4kehDLI6z
+eM7b41N5cdblIZQB2lWHmiRk9opmzN6cN82oNLFpmyPInngiK3BD41VHMWEZ71jF
+hS9OMPagMRYjyOfiZRYzy78aG6A9+MpeizGLYAiJLQwGXFK3xPkKmNEVX58Svnw2
+Yzi9RKR/5CYrCsSXaQ3pjOLAEFe4yHYSkVXySGnYvCoCWw9E1CAx2/S6cCZdkGCe
+vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep
++OkuE6N36B9K
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ
+RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD
+VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX
+DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y
+ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy
+VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr
+mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr
+IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK
+mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu
+XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy
+dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye
+jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1
+BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3
+DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92
+9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx
+jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0
+Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz
+ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS
+R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp
+-----END CERTIFICATE-----
+CERT1
+  
+  if type wget >/dev/null 2>&1; then
+    wget -q -O ${output_dir}/nuget.exe ${URL} --ca-certificate=/tmp/nugetroot.pem
+  else
+  if type curl >/dev/null 2>&1; then
+  	curl -# -L --cacert ${cert_dir}/nugetroot.pem ${URL} > ${output_dir}/nuget.exe 
+  else
+  	/usr/bin/fetch -o ${output_dir}/nuget.exe --ca-cert=${cert_dir}/nugetroot.pem ${URL}
+  fi; fi
+}
+
+nugetdownload "${TOOLSDIR}"
+main
